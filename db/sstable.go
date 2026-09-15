806b6f8f628cac3b42c2fb58abede5f07489c5a1
Pratik <pratiky2000@gmail.com>
2026-09-15 23:33:52 +0530

    close the file

--- db/sstable.go
+++ db/sstable.go
@@ -12,9 +12,12 @@ func Flush(mem *memtable.MemTable) error {
 	keys := []string{}
 
 	file,err := os.OpenFile("./data/seg.txt",os.O_CREATE | os.O_WRONLY | os.O_TRUNC,0644)
-	if err != nil{
+
+	if err != nil {
 		return err
 	}
+	
+	defer file.Close()
 
 	fmt.Println(file)
 	for key := range mem.Entries(){
@@ -26,7 +29,11 @@ func Flush(mem *memtable.MemTable) error {
 	for _,key := range keys {
 		value := mem.Entries()[key]
 
-		file.Write([]byte(key + "|" + string(value) + "\n"))
+		_,err := file.Write([]byte(key + "|" + string(value) + "\n"))
+
+		if err != nil {
+			return err
+		}
 	}
 
 	fmt.Println(keys)
